71bc016b1e08d8fbdebfa12a335c8fe21be24a41
Hugoramirez1 <ramirez.huguito@gmail.com>
2025-01-21 17:22:42 -0300

    Se agregan archivos faltantes

--- app-1.js
+++ app-1.js
@@ -1,6 +1,8 @@
-console.log(4+10);
-
-console.log(2*2+6);
-
-console.log(11-23);
-
+console.log(4 + 10);
+// puntos de interrupción o breakpoints
+//debugger;
+alert(4 + 10);
+//debugger;
+console.log(10-23);
+//debugger;
+alert(2*2+6);
