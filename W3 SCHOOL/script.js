548739ddb73b327b34a9fb93c741da44cc1f1d3e
hugo.ramirez1 <you@example.com>
2025-03-15 09:43:22 -0300

    se agregan funciones (practica botones)

--- W3 SCHOOL/script.js
+++ W3 SCHOOL/script.js
@@ -1,16 +1,20 @@
 function myFunction() {
     document.getElementById("hiddenText").style.display = "block";
-    document.getElementById("secbtn").style.display="block";
+    document.getElementById("secbtn").style.display = "block";
+    document.getElementById("btn").style.display = "none";
 }
 
 function myFunction2() {
     document.getElementById("hiddenText").innerHTML = "Con este botón cambias el texto";
-    document.getElementById("secbtn").style.display="none";
-    
+    document.getElementById("secbtn1").style.display = "block";
+    document.getElementById("secbtn").style.display = "none";
+
 
 }
-function myFunction3(){
-    document.getElementById("hiddenTex2").innerHTML = "Con este botón sigues cambiando texto";    
+function myFunction3() {
+    document.getElementById("hiddenText2").style.display = "block";
+    document.getElementById("hiddenText2").innerHTML = "Con este botón sigues cambiando texto";
+    document.getElementById("hiddenText").style.display = "none";
 }
 
 
