6fb76b6d3846ab7ba639e29aa00c1cf5ccc5556c
hugo.ramirez1 <you@example.com>
2025-03-15 07:58:11 -0300

    Se agrega practica con W3School

---
+++ W3 SCHOOL/script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    document.getElementById("hiddenText").style.display = "block";
+    document.getElementById("secbtn").style.display="block";
+}
+
+function myFunction2() {
+    document.getElementById("hiddenText").innerHTML = "Con este botón cambias el texto";
+    document.getElementById("secbtn").style.display="none";
+    
+
+}
+function myFunction3(){
+    document.getElementById("hiddenTex2").innerHTML = "Con este botón sigues cambiando texto";    
+}
+
+
