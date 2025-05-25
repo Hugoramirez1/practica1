b66e5e07a050d9d6b99e7e0ccbd035bbb381edb1
Hugoramirez1 <ramirez.huguito@gmail.com>
2025-05-25 19:06:49 -0300

    Encapsulamiento PUBLICO

---
+++ INFO3/Encapsulamiento/encapsulamiento.java/src/Encapsulamiento.java
@@ -0,0 +1,7 @@
+public class Encapsulamiento {
+    public static void main(String[] args) {
+        empleado empe = new empleado ();
+        empe.nombre = "Hugo";
+        System.out.println("El nombre del empleado es:"+ empe.nombre);
+    }
+}
---
+++ INFO3/Encapsulamiento/encapsulamiento.java/src/empleado.java
@@ -0,0 +1,3 @@
+public class empleado {
+    public String nombre;
+}
